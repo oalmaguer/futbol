f189f268a49dfd9bbb73480aef55478eab2bdd46
oalmaguer <almaguero95@gmail.com>
2020-09-26 21:19:45 -0400

    betting done

--- app.js
+++ app.js
@@ -12,30 +12,124 @@ $(document).ready(function(){
     $(".hey").click(function() {
 
         $("#compara").empty();
+        $("#ultimosjuegos").empty();
+
+        
+        var puntosL = 0;
+        var puntosV = 0;
+
+        var comparacionL = 0;
+        var comparacionV = 0;
+
 
         var equipoLocal = $("#local").val();
         var equipoVisitante = $("#visitante").val();
 
-        var equipoL = $("#local").text();
-        var equipoV = $("#visitante").text();
+        var equipoL = $("#local option:selected").text();
+        var equipoV = $("#visitante option:selected").text();
 
+        residuo = 0;
 
         console.log(equipoLocal);
         console.log(equipoVisitante);
 
 
+
+
+
        $.get(`http://livescore-api.com/api-client/teams/head2head.json?team1_id=${equipoLocal}&team2_id=${equipoVisitante}&key=5YruetBzlYuM39j5&secret=trQ35goOgMvxFZWLXnfcQQMcPgYSqiXT`, function(data, status) {
            console.log(data);
-           for (i=0;i<=10;i++) {
-            $("#compara").append(`
+
+        for (i=0;i<7;i++) {
+            if (data.data.team1.overall_form[i] == "W") {
+                puntosL = puntosL + 3;
+            } else if (data.data.team1.overall_form[i] == "D") {
+                puntosL = puntosL + 1;
+            }
+    
+            if (data.data.team2.overall_form[i] == "W") {
+                puntosV = puntosV + 3;
+            } else if (data.data.team2.overall_form[i] == "D") {
+                puntosV = puntosV + 1;
+            }
+    
+        }
+           
+        console.log("Puntos local " + puntosL);
+        console.log("Puntos Visitante " +  puntosV);
+
+            for (i=0;i<6;i++) {
+                if (data.data.team1.h2h_form[i] == "W") {
+                    comparacionL = comparacionL + 3;
+                } else if (data.data.team1.h2h_form[i] == "D") {
+                    comparacionL = comparacionL+1;
+                }
+
+                if (data.data.team2.h2h_form[i] == "W") {
+                    comparacionV = comparacionV + 3;
+                } else if (data.data.team2.h2h_form[i] == "D") {
+                    comparacionV = comparacionV + 1;
+                }
+            }
+                console.log("Puntos Compración local " + comparacionL);
+                console.log("Puntos Compración Visitante " + comparacionV);
+
+                totalL = puntosL + comparacionL;
+                totalV = puntosV + comparacionV;
+
+                totalL = totalL /2;
+                totalV = totalV /2;
+
+
+                
+
+                if (totalL > totalV) {
+                    residuo = totalL - totalV;
+                    console.log("total local " + totalL)
+                    console.log("total visitante " + totalV)
+                    console.log("residuo " + residuo);
+                } else {
+                    console.log("total local " + totalL)
+                    console.log("total visitante " + totalV)
+                    residuo = totalV - totalL;
+                    console.log("residuo " + residuo);
+                }
+
+                if (residuo < 4) {
+                   alert("Empate");
+                } else if (residuo >= 4) {
+                    if (totalL > totalV) {
+                       alert("Probable ganador es " + equipoL);
+                    } else {
+                       alert("Probable ganador es " + equipoV);
+                    }
+                }
+
+
+           $("#compara").append(`
+           <thead>
+           <tr>
+           <th>Equipo Local</th>
+           <th>marcador</th>
+           <th>Equipo Visitante</th>
+           <th>Fecha</th>
+           </tr>
+           </thead>`)
+
+           $("#ultimosjuegos").append(`
+           
             <thead>
             <tr>
-            <th>Equipo Local</th>
-            <th>marcador</th>
-            <th>Equipo Visitante</th>
-            <th>Fecha</th>
+            <th>${equipoL}</th>
+            <th>${equipoV}</th>
             </tr>
             </thead>
+           `)
+
+
+           for (i=0;i<=10;i++) {
+            $("#compara").append(`
+            
             <tbody>
             <td>${data.data.h2h[i].home_name}</td>
             <td>${data.data.h2h[i].score}</td>
@@ -44,20 +138,22 @@ $(document).ready(function(){
             </tr>
             </tbody>
             `)
+
            
             $("#ultimosjuegos").append(`
-         
-            <tr>
             <tbody>
-            <td>${data.data.team1.h2h_form[i]}</td>
-            <td>${data.data.team2.h2h_form[i]}</td>
+            <td>${data.data.team1.overall_form[i]}</td>
+            <td>${data.data.team2.overall_form[i]}</td>
+           
             </tr>
             </tbody>
             `)
 
+           
+
            }
 
-          
+           
           
 
         })
